e1ad794524e3f6b3566ede127050a749d4796d2a
Rage1904 <esiedesac@gmail.com>
2022-10-16 07:17:23 -0700

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: 0 (Correct)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 2; i++)
+
+		for (j = 1; j <= 2; j++)
+
+			printf("%d %d\n", i, j);
+	return (0);
+}
