9bb8dae2dc14ed4d6efd093f728b8f7aed87613f
Rage1904 <esiedesac@gmail.com>
2022-10-16 08:32:10 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,12 +7,33 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, k = '0', j;
 
-	for (i = 1; i <= 2; i++)
-
-		for (j = 1; j <= 2; j++)
-
-			
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar('');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
 	return (0);
 }
