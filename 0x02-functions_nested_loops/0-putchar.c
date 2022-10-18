a65f23863d4f12f36f5bb6dfcf2a37d4bcdbc6bd
Rage1904 <esiedesac@gmail.com>
2022-10-18 12:12:37 -0700

    main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - putchar
+ * Description: print putchar followed by anew line
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putcher('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
