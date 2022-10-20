5965387adc1e22c99edab85001c58c0b36560677
Rage1904 <esiedesac@gmail.com>
2022-10-20 03:04:35 -0700

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - comment the bug
+ * Return: 0 (Correct)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * }
+	 * putchar(1);
+	 * )
+	 */
+
+	printf("infinite loop avoided! \\o/\");
+
+	return (0);
+}
