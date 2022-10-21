ec0d4fea0c19471c2e510098e5dd0c0507ad3a57
Rage1904 <esiedesac@gmail.com>
2022-10-21 10:17:59 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - a program to solve fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3) == 0 && (a % 5) != 0)
+			printf("Fizz ");
+		else if ((a % 5) == 0 && (a % 3) != 0 && a != 100)
+			printf("Buzz ");
+		else if ((a % 5) == 0 && (a % 3) != 0 && a == 100)
+			printf("Buzz ");
+		else if ((a % 3) == 0 && (a % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+
+	return (0);
+}
