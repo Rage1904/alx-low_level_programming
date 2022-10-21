bbf6d743085b962245d02f115d1cc71d8c3163dc
Rage1904 <esiedesac@gmail.com>
2022-10-21 10:22:53 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 		else if ((a % 5) == 0 && (a % 3) != 0 && a != 100)
 			printf("Buzz ");
 		else if ((a % 5) == 0 && (a % 3) != 0 && a == 100)
-			printf("Buzz ");
+			printf("Buzz");
 		else if ((a % 3) == 0 && (a % 5) == 0)
 			printf("FizzBuzz ");
 		else
