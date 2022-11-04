afa9547dc9f371d10efdcceea714b7cf81221a44
Rage1904 <esiedesac@gmail.com>
2022-11-04 09:07:50 -0700

    tk1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: command line count
+ * @argv: command value
+ * Return: o
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
