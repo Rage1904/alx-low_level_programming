78cb81a537efb954a1cbe5cfbea1d02523f161bf
Rage1904 <esiedesac@gmail.com>
2022-11-10 08:55:33 -0800

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	n = malloc(sizeof(*n) * ((min - max) + 1));
+	n = malloc(sizeof(*n) * ((max - min) + 1));
 
 	if (n == NULL)
 		return (NULL);
