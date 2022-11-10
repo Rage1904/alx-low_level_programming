59d48d6dcb449337dc5d2ab08c44b7edf485be52
Rage1904 <esiedesac@gmail.com>
2022-11-10 08:37:10 -0800

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (cal == NULL)
 		return (NULL);
 
-	for (a = 0; a < (nmemb * size); a++)
+	for (a = 0; a < (sizeof(nmemb * size)); a++)
 		cal[a] = 0;
 
 	return (cal);
