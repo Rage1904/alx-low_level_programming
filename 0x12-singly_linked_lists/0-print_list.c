fccb0517761fb4810292ed9d49b6cbe43cd603c0
Rage1904 <esiedesac@gmail.com>
2022-11-25 15:52:44 -0800

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* print_list - a function that prints all elements of a singly-linked list
+* @h: a pointer to the element
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
