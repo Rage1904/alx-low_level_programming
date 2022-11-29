1a41d27c79de216c9ca237922ef23ce0f0595ab9
Rage1904 <esiedesac@gmail.com>
2022-11-28 23:36:09 -0800

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	tmp = *head;
+	rad = *head;
 
 	if (*head == NULL)
 	{
